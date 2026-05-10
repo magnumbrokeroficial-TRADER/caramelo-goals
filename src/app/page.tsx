9d623d0b489cbba5d1811e8784b57f75d612d74a
magnumbroker <magnumbroker@local>
2026-05-10 01:33:48 -0300

    Força HTML estático

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import Dashboard from '@/components/dashboard/Dashboard';
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-[#0a0a0a] text-gray-200">
-      <Dashboard />
-    </main>
-  );
-}
